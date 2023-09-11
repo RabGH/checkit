42d1e46b772a01b1cf72559f5d4a8aa5ea53e7d6
RabGh <rabiighais@gmail.com>
2023-09-12 01:30:04 +0300

    Textarea from shadcn & create task dialog form

--- components/dashboard/create-task-dialog.tsx
+++ components/dashboard/create-task-dialog.tsx
@@ -1,15 +1,33 @@
 "use client";
 
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
 import { cn } from "@/lib/utils";
 import { Collection } from "@prisma/client";
 import { CollectionColor, CollectionColors } from "@/util/constants";
+import {
+  CreateTaskValidator,
+  CreateTaskValidatorType,
+} from "@/validators/create-task-validator";
 
 import {
   Dialog,
   DialogContent,
+  DialogDescription,
   DialogHeader,
   DialogTitle,
 } from "@/components/ui/dialog";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
+import { Textarea } from "@/components/ui/textarea";
+import { Popover } from "../ui/popover";
 
 interface CreateTaskDialogProps {
   open: boolean;
@@ -22,16 +40,27 @@ function CreateTaskDialog({
   collection,
   setOpen,
 }: CreateTaskDialogProps) {
+  const form = useForm<CreateTaskValidatorType>({
+    resolver: zodResolver(CreateTaskValidator),
+    defaultValues: {
+      collectionId: collection.id,
+    },
+  });
+
   const openChangeWrapper = (value: boolean) => {
     setOpen(value);
   };
 
+  const onSubmit = async (data: CreateTaskValidatorType) => {
+    console.log("SUBMITTED", data);
+  };
+
   return (
     <Dialog open={open} onOpenChange={openChangeWrapper}>
-      <DialogContent>
+      <DialogContent className="sm:max-w-screen-sm">
         <DialogHeader>
           <DialogTitle>
-            Add task to collection:{" "}
+            Add task to collection:
             <span
               className={cn(
                 "p-[1px] bg-clip-text text-transparent",
@@ -41,7 +70,50 @@ function CreateTaskDialog({
               {collection.name}
             </span>
           </DialogTitle>
+          <DialogDescription>
+            Add a task to collection, add as many as you want.
+          </DialogDescription>
         </DialogHeader>
+        <div>
+          <Form {...form}>
+            <form
+              className="space-y-4 flex flex-col"
+              onSubmit={form.handleSubmit(onSubmit)}
+            >
+              <FormField
+                control={form.control}
+                name="content"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Expires at</FormLabel>
+                    <FormControl>
+                      <Textarea
+                        {...field}
+                        rows={5}
+                        placeholder="Type your task here..."
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="expiresAt"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Expire Date</FormLabel>
+                    <FormDescription>
+                      When will the task expire?
+                    </FormDescription>
+                    <FormControl>
+                      <Popover></Popover>
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+            </form>
+          </Form>
+        </div>
       </DialogContent>
     </Dialog>
   );
