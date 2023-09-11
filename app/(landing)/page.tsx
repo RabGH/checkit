9df7a85bbed0bc92515bcf77bc6ce5ba7310a8aa
RabGh <rabiighais@gmail.com>
2023-09-11 14:00:17 +0300

    Add landing page temp

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,5 +1,15 @@
+import { LandingNavbar } from "@/components/landing/landing-navbar";
+import { LandingHero } from "@/components/landing/landing-hero";
+import { LandingContent } from "@/components/landing/landing-content";
+
 const LandingPage = () => {
-  return <div>LandingPage</div>;
+  return (
+    <div className="h-full">
+      <LandingNavbar />
+      <LandingHero />
+      <LandingContent />
+    </div>
+  );
 };
 
 export default LandingPage;
---
+++ components/landing/landing-content.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+export const LandingContent = () => {
+  return (
+    <div className="px-10 pb-20">
+      <div className="text-center mb-10">
+        <h2 className="text-4xl text-white font-extrabold mb-2">Features</h2>
+        <p className="text-zinc-400 text-xs md:text-sm font-normal">
+          Still in Alpha.
+        </p>
+      </div>
+    </div>
+  );
+};
