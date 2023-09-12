a5c1f4363d58cf780c927c0a925e7b211c07a84c
RabGh <rabiighais@gmail.com>
2023-09-12 04:34:07 +0300

    Add CheckIt link

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,13 +1,17 @@
-import React from "react";
+"use client";
+
+import Link from "next/link";
 
 const Logo = () => {
   return (
-    <h1
-      className="text-2xl font-bold bg-gradient-to-r 
+    <Link href="/dashboard">
+      <h1
+        className="text-2xl font-bold bg-gradient-to-r 
     from-red-700 via-red-600 to-red-500 bg-clip-text text-transparent"
-    >
-      CheckIt
-    </h1>
+      >
+        CheckIt
+      </h1>
+    </Link>
   );
 };
 
