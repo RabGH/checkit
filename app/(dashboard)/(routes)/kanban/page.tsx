99a585d5777326759c3bf5717d287b87b8234143
RabGh <rabiighais@gmail.com>
2023-11-19 22:29:41 +0200

    remove suspence

--- app/(dashboard)/(routes)/kanban/page.tsx
+++ app/(dashboard)/(routes)/kanban/page.tsx
@@ -4,7 +4,6 @@ import { Suspense } from "react";
 import WelcomeMsg from "@/components/dashboard/welcome";
 import WelcomeMsgFallback from "@/components/dashboard/welcome-fallback";
 import KanbanBoard from "@/components/kanban/kanban-board";
-import KanbanList from "@/components/kanban/kanban-list";
 
 const KanbanBoardPage = () => {
   return (
@@ -12,9 +11,8 @@ const KanbanBoardPage = () => {
       <Suspense fallback={<WelcomeMsgFallback />}>
         <WelcomeMsg />
       </Suspense>
-      <Suspense fallback={<div>Loading columns...</div>}>
-        <KanbanBoard />
-      </Suspense>
+
+      <KanbanBoard />
     </>
   );
 };
